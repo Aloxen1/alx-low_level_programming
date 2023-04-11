d8cbc8c82d873e9d79d9e5c5b6552a7704d3c1a3
Aloxen1 <108733364+Aloxen1@users.noreply.github.com>
2023-04-11 15:20:47 -0700

    Update 0-memset.c

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,3 +1,18 @@
-0-memset.c
+#include "main.h"
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <ctype.h>
+
+#include <string.h>
 
 
+char *_memset(char *s, char b, unsigned int n)
+
+{
+
+    return memset(s, b, n);
+
+}
