305a363cb7919e27080998efab955adfb7746977
Aloxen1 <108733364+Aloxen1@users.noreply.github.com>
2023-04-11 15:18:03 -0700

    Update 1-isdigit.c

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,3 +1,18 @@
-1-isdigit.c
+#include "main.h"
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <ctype.h>
+
+#include <string.h>
 
 
+int _isdigit(int c)
+
+{
+
+    return isdigit(c);
+
+}
