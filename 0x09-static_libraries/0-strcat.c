5d6b3059a22132a6090ec0473aacc4c2fb451a81
Aloxen1 <108733364+Aloxen1@users.noreply.github.com>
2023-04-11 15:19:24 -0700

    Update 0-strcat.c

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,3 +1,18 @@
-0-strcat.c
+#include "main.h"
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <ctype.h>
+
+#include <string.h>
 
 
+char *_strcat(char *dest, char *src)
+
+{
+
+    return strcat(dest, src);
+
+}
