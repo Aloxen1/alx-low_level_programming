2dfb2e30bc9e77be75d4466bd4c9bbb13cc36fde
Aloxen1 <108733364+Aloxen1@users.noreply.github.com>
2023-04-11 15:18:55 -0700

    Update 9-strcpy.c

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,3 +1,17 @@
-9-strcpy.c
+#include "main.h"
 
+#include <stdio.h>
 
+#include <stdlib.h>
+
+#include <ctype.h>
+
+#include <string.h>
+
+char *_strcpy(char *dest, char *src)
+
+{
+
+    return strcpy(dest, src);
+
+}
