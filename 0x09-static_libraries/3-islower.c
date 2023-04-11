b7f1febe74c88b13b4b9df9a0d0acc595f1fb47f
Aloxen1 <108733364+Aloxen1@users.noreply.github.com>
2023-04-11 15:16:46 -0700

    Update 3-islower.c

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,1 +1,18 @@
-3-islower.c
+#include "main.h"
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <ctype.h>
+
+#include <string.h>
+
+
+int _islower(int c)
+
+{
+
+    return (c >= 'a' && c <= 'z');
+
+}
