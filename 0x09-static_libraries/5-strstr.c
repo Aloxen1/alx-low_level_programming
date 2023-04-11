3a9d6a21261d8058f83e07aa27735f0c30f57216
Aloxen1 <108733364+Aloxen1@users.noreply.github.com>
2023-04-11 15:22:19 -0700

    Update 5-strstr.c

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,3 +1,18 @@
-5-strstr.c
+#include "main.h"
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <ctype.h>
+
+#include <string.h>
 
 
+char *_strstr(char *haystack, char *needle)
+
+{
+
+    return strstr(haystack, needle);
+
+}
