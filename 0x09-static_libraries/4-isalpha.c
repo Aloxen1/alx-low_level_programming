10a8b649c86cd43f9e4929518ae0777c9d105619
Aloxen1 <108733364+Aloxen1@users.noreply.github.com>
2023-04-11 15:17:02 -0700

    Update 4-isalpha.c

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,3 +1,19 @@
-4-isalpha.c
+#include "main.h"
 
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <ctype.h>
+
+#include <string.h>
+
+
+int _isalpha(int c)
+
+{
+
+    return isalpha(c);
+
+}
 
