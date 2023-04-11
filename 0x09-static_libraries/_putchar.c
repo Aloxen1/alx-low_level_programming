f7876be71aaf893ebcb9367789fc3758200c4a76
Aloxen1 <108733364+Aloxen1@users.noreply.github.com>
2023-04-11 15:16:19 -0700

    Update _putchar.c

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,1 +1,18 @@
-_putchar.c
+#include "main.h"
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <ctype.h>
+
+#include <string.h>
+
+
+int _putchar(char c)
+
+{
+
+    return putchar(c);
+
+}
